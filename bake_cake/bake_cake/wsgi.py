e396d6cb624000a567db1bf5bc1b64437bef0204
Fiskless <Fiskless@gmail.com>
2021-11-01 11:54:56 +0000

    Modify wsgi

--- bake_cake/bake_cake/wsgi.py
+++ bake_cake/bake_cake/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from dotenv import load_dotenv
+from environs import Env
+env = Env()
+env.read_env()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bake_cake.settings')
+project_folder = os.path.expanduser('~/PycharmProjects/Github projects/BakeCake')
+load_dotenv(os.path.join(project_folder, '.env'))
 
 application = get_wsgi_application()
