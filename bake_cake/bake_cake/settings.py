74b3b4b13eee0ac959bf31cec12432d3d15a70df
Fiskless <Fiskless@gmail.com>
2021-11-01 18:14:28 +0500

    Change settings

--- bake_cake/bake_cake/settings.py
+++ bake_cake/bake_cake/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
@@ -133,3 +133,9 @@ AUTH_USER_MODEL = 'cakesite.CustomUser'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATIC_URL = '/assets/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
